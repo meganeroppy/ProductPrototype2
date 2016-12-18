e5b396abc94ffcb91de764c7a040e25a9f1066c3
m.roppy <roppy@hotmail.co.jp>
2016-12-18 18:11:36 +0900

    仮シーン作成

---
+++ Assets/UIController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+
+	public Button button;
+
+	public Text buttonLabel;
+
+	private bool connected;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// 現在の状態を取得
+		StartCoroutine( APIManager.instance.GetState( res =>
+		{
+				connected = res;
+		}));
+	}
+
+
+	public void PressConnectButton()
+	{
+		bool newState = !connected;
+
+		StartCoroutine( APIManager.instance.SetState(newState, res => 
+			{
+				if(res)
+				{
+					connected = res;
+				}	
+		}) );
+	}
+
+}
